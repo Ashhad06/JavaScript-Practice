aeaba052a86ae19e06839cd6c4679d0796154f41
Ashhad06 <ashhadchaghtai07@gmail.com>
2023-04-06 15:54:11 +0500

    2 Question done

---
+++ Q1.js
@@ -0,0 +1,8 @@
+let a = +prompt("Enter your first number")
+let b = +prompt("Enter your second number")
+if (a>b){
+    console.log(a + " is greater than " + b)
+}
+else {
+    console.log(b + " is greater than " + a)
+}
---
+++ Q2.js
@@ -0,0 +1,7 @@
+let a = +prompt("Enter you number");
+if (a >= 0) {
+    alert("The sign is +")
+}
+else{
+    alert("The sign is -")
+}
